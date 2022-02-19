598843a9793452ff2dd9bc9d57835d56248e830c
José Nelson Cultri <josefcultrifrc@gmail.com>
2022-02-18 22:33:55 -0300

    Correção de problema quando redefinia a casa inicial para qualquer valor de casa não válido

--- engine.js
+++ engine.js
@@ -6,6 +6,7 @@ var casaJaPercorridaBadgeHtml = '<span class="indicador-casa indicador-casa-visi
 var casaAtualBadgeHtml = '<span class="indicador-casa indicador-casa-atual">{0}</span>';
 var mapeamentoLetraCasa = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8 };
 var casasJaPercorridas = [];
+var casasPossiveis = ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8"];
 
 document.addEventListener("DOMContentLoaded", function(){
     iniciarPasseio();
@@ -102,5 +103,10 @@ function iniciarPasseio() {
 
 function redefinirCasaInicial() {
     casaInicial = prompt("Digite a nova casa inicial: ");
+    if (
+        casasPossiveis.indexOf(casaInicial) == -1
+    ) {
+        casaInicial = "a1";
+    }
     reiniciarPasseio();
 }
